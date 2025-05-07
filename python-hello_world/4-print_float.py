3510a900bdc148a72be98defa429733f8a56d23a
Kassandra <kassandraiatrou@gmail.com>
2025-05-07 12:47:43 +1000

    task 3 is printing a string 3 times and the first 9 characters of the string followed by a new line

--- python-hello_world/4-print_float.py
+++ python-hello_world/4-print_float.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 number = 3.14159
 print(f"Float: {number:.2f}")
+
+# {number:.2f} means:
+# number - the variable
+# :.2f - format as a float with 2 digits after the decimal point
