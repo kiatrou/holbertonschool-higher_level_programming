098c269d46ab367c7f5cbf0be7668c26c91cad26
Kassandra <kassandraiatrou@gmail.com>
2025-05-07 11:23:20 +1000

    added comments to task 1 code

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 number = 98
 print(f"{number} Battery street")
+
+# An f string (formatted string literalts) is used to simplify formatting and interpolation
+# {variable you want to use}
