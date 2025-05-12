6a4c90c6bb6ee9870109cf9ae32f497f0b77494b
Kassandra <kassandraiatrou@gmail.com>
2025-05-12 23:25:58 +1000

    time for pycodestyle to be the new betty

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 letter = 97
 while letter < 122:
-    print(f"{chr(letter)}", end="")
+    print("{}".format(chr(letter)), end="")
     letter += 1
 
 # using chr ensures the program prints the ASCII value, not the number
