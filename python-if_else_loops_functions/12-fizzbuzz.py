c97191b8c4bc13c14dc8de8d7d8900fd9cdc292d
Kassandra <kassandraiatrou@gmail.com>
2025-05-13 15:16:35 +1000

    task 12 is the FizzBuzz task

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    hundred = 1
+
+    while hundred <= 100:
+        if (hundred % 3) == 0 and (hundred % 5) == 0:
+            print("FizzBuzz", end=" ")
+        elif hundred % 3 == 0:
+            print("Fizz", end=" ")
+        elif hundred % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print("{}".format(hundred), end=" ")
+        hundred += 1
+
+fizzbuzz()
