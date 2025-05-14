92f792b798da2a32a7d3326f858102725166e661
Kassandra <kassandraiatrou@gmail.com>
2025-05-14 11:09:28 +1000

    task 2 is a program that prints the number of and the list of its arguments

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 import sys
 
-counter = 0
+if __name__ == "__main__":
+    counter = 0
 
-for i in sys.argv:
-    counter += 1
-    print("{} arguments:".format(counter))
-    print("{}: {}".format(counter, i))
-    if i == 0:
-        print("{} arguments.".format(counter))
+    for i in sys.argv:
+        counter += 1
+        print("{} arguments:".format(counter))
+        print("{}: {}".format(counter, i))
+        if i == 0:
+            print("{} arguments.".format(counter))
