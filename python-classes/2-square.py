c9577d583421a5a36aa74d6dbaec69f94b6ee75a
Kassandra <kassandraiatrou@gmail.com>
2025-05-22 05:49:24 +0000

    task 2 is a class square with error checks

---
+++ python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This module defines an empty Square class."""
+
+
+class Square:
+    """
+    A class to represent a square.
+
+    Attributes
+    ----------
+    size : int
+        size of the square
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
