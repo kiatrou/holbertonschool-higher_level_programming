6ec7f91ddc9d77f93a5d036dd206b927eb7cf38b
Kassandra <kassandraiatrou@gmail.com>
2025-05-22 05:37:21 +0000

    task 1 is a class square with the size attribute added

---
+++ python-classes/1-sqaure.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module defines an empty Square class."""
+
+
+class Square:
+    """
+    A class to represent a square.
+
+    Attributes
+    ----------
+    size : int
+        size of the square
+    """
+    def __init__(self, size):
+        self.__size = size
