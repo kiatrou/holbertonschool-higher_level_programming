8316090184eea4c436c101586ec1cecd22b866f9
Kassandra <kassandraiatrou@gmail.com>
2025-05-23 04:13:39 +0000

    task 2 adds the area and perimeter methods to rectangle class

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""This module defines an empty Rectangle class."""
+
+
+class Rectangle:
+    """
+    A class to represent a rectangle.
+
+    Attributes
+    ----------
+    width(self):
+        Returns value of private attribute __width
+    height(self):
+        Returns value of private attribute __height
+
+    Methods
+    -------
+    area(self):
+        returns the area of a rectangle
+    perimeter(self):
+        returns the rectangle perimeter
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return (self.width * self.height)
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            self.perimeter = 0
+        return ((self.width + self.height) * 2)
