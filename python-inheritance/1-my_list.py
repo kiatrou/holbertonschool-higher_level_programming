13b581459347c8329702caf3b34a918c8e7cc033
Kassandra <kassandraiatrou@gmail.com>
2025-05-26 14:37:17 +1000

    task 1 is a class, MyList that inherites from list and prints a sorted list in ascending order

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module has the class MyList which inherites
+from list
+"""
+
+
+class MyList(list):
+    """
+    A class to represent my list
+
+    Attributes
+    ----------
+
+    Methods
+    ----------
+    print_sorted:
+        prints the list in ascending order
+    """
+    def print_sorted(self):
+        print(sorted(self))
