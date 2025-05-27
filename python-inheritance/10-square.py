00df25714a1935d8979301401159e1ebbd678f36
Kassandra <kassandraiatrou@gmail.com>
2025-05-27 04:11:50 +0000

    task 10 creates a class square

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This is a class Rectangle
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Class sqaure
+
+    Attributes
+    ----------
+    size:
+        size of a sqaure
+    """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return (self.__size ** 2)
