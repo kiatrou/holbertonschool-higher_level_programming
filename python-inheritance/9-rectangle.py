b4b3892a17a060c80ea78f03671642fedaae9f68
Kassandra <kassandraiatrou@gmail.com>
2025-05-27 03:11:32 +0000

    task 9 overrides area() and print methods

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -16,9 +16,23 @@ class Rectangle(BaseGeometry):
         private - width of rectangle
     height:
         private - height of rectangle
+
+    Methods
+    ----------
+    area(self):
+        returns the area of a rectangle
+    __str__(self):
+        defines what gets returned when you use str() or print() on an object
+        for this class, return "[Rectangle] {width}/{height}"
     """
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
+
+    def area(self):
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
