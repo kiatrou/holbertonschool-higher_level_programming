cd79cb124cf2205afb7c0e34a767e3627ac4abb8
Kassandra <kassandraiatrou@gmail.com>
2025-05-28 00:26:49 +0000

    task 2 prints first and last name

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,17 @@
+#!/usr/bin/pyhton3
+"""
+This is a function that prints out a first and last name.
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    This function takes in two arguments - first name and last name
+    last name will default to empty string if not filled
+    prints "My name is <firtst name> <last name>
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print(f"My name is {first_name} {last_name}")
