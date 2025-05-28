134268e20b9eb7c01eb9b7f58d9659520f57e814
Kassandra <kassandraiatrou@gmail.com>
2025-05-28 04:11:28 +0000

    task 3 prints a square

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This function prints a square
+"""
+
+
+def print_square(size):
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        print("#" * size)
