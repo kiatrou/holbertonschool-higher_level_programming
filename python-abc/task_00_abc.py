2cf6948ccf5347280d9959f13d337853c8afd696
Kassandra <kassandraiatrou@gmail.com>
2025-05-29 16:27:13 +1000

    task 0 uses ABC class to make animals

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+ABC classes
+"""
+
+
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    """
+    Abstract class
+
+    Methods
+    ----------
+    sound:
+        the sound the animal makes
+    """
+    @abstractmethod
+    def sound(self):
+        pass
+
+
+class Dog(Animal):
+    """
+    Subclass of Animal
+    """
+    def sound(self):
+        print("Bark")
+
+
+class Cat(Animal):
+    """
+    Sublass of Animal
+    """
+    def sound(self):
+        print("Meow")
