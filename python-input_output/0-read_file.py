9977c1b3cfbda2396b860ab3ee376082c4dc7bcd
Kassandra <kassandraiatrou@gmail.com>
2025-06-10 10:34:18 +1000

    fixed checker error

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""
+This reads a file and prints it to the standard output
+"""
+
+
 def read_file(filename=""):
+    """
+    Using the with statement ensures that setup and takedown is
+    automatically done
+    """
     with open(filename, encoding="utf-8") as file:
         contents = filename.read()
