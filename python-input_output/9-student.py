0962ec111b2d3a35cd137bd15801332123af0452
Kassandra <kassandraiatrou@gmail.com>
2025-06-11 16:46:47 +1000

    task 9 is a class student

---
+++ python-input_output/9-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This is class Student
+"""
+
+
+class Student:
+    """
+     A class to represent a student.
+
+    Attributes
+    ----------
+    first_name:
+        student's first name (public)
+    last_name:
+        student's last name (public)
+    age:
+        student's age (public)
+
+    Methods
+        to_json:
+            retrieves a dictionary representation of a Student instance
+    -------
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return (Student.__dict__)
