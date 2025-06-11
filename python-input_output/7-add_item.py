640eced2244e34081f8ed90acbe55a07724a8d88
Kassandra <kassandraiatrou@gmail.com>
2025-06-11 16:12:32 +1000

    task 7 is a script that adds all arguments to a python list and then save them to a file

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This is a script that adds all arguments to a Python list
+and then saves them to a file
+"""
+
+
+import sys
+import json
+# Import the save_to_json_file and load_from_json_file functions
+# using __import__ because the files have numeric prefixes
+save_to_json = __import__('5-save_to_json').save_to_json
+load_from_json = __import__('6-load_from_json').load_from_json
+
+
+# Using 1: ensures that only the arguments are used and not the filename,
+# which is at index 0
+# use sys.argv to accept input from terminal
+arguments_being_added = sys.argv[1:]
+# Define the filename where the list will be saved and loaded from
+filename = "add_item.json"
+try:
+    # Try to load the existing list from the JSON file
+    current_list = load_from_json(filename)
+except FileNotFoundError:
+    # create a new list if one isn't found
+    current_list = []
+# Add the new command-line arguments to the existing list
+current_list.extend(arguments_being_added)
+# Save the updated list back to the JSON file
+save_to_json(current_list, filename)
