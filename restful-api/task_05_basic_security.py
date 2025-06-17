6d5273480e4cacf79141411fdf72d8e9409f600d
Kassandra <kassandraiatrou@gmail.com>
2025-06-17 12:23:30 +1000

    api security and authentication techniques

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -37,9 +37,9 @@ def verify_password(username, password):
             check_password_hash(users[username]["password"], password):
         return (users[username])
 
-@auth.error_response
-def auth_error_response():
-    return (jsonify({"error": "Unauthorized access"}), 401)
+@auth.error_handler
+def unauthorized():
+    return jsonify({"error": "Unauthorized"}), 401
 
 
 # basic protected creates a route available at /basic-protected
