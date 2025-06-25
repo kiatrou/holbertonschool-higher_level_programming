ca25b3379838eaf6ed06209276dd701f4c0b9ead
Kassandra <kassandraiatrou@gmail.com>
2025-06-26 09:29:35 +1000

    task 2 deletes a database from MySQL server

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,2 +1,2 @@
--- This is a script that deletes the database hbtn_0c_0 in my MySQL server
+-- This is a script that deletes the database hbtn_0c_0 in MySQL server
 DROP DATABASE IF EXISTS hbtn_0c_0;
