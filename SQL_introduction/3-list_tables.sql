620ce036bf050f5ec48dcf28cc6c09a57243de33
Kassandra <kassandraiatrou@gmail.com>
2025-06-26 09:40:06 +1000

    task 3 lists all tables of a database in mysql server

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,5 +1,2 @@
 -- This is a script that lists all the tables of a database in MySQL server
-SELECT table_name
-FROM information_schema.tables
-WHERE table_type='BASE TABLE'
-    AND table_schema = 'mysql'
+SELECT * FROM mysql.sys.Tables;
