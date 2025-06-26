6ee0775744a10041dc48bacba7bde5d890072144
Kassandra <kassandraiatrou@gmail.com>
2025-06-26 10:15:26 +1000

    task 4 creates a table in the current database in MySQL server

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- This script creates a table in the current database in MySQL server
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+)
+-- Note: VARCHAR = variable length string (size of string)
