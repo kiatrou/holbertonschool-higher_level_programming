6cd364cf4af3964740c99c067db2fc57fd6819e9
Kassandra <kassandraiatrou@gmail.com>
2025-06-30 11:00:27 +1000

    task 1 creates a user in MySQL

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- This is a scrip that creates a user in MySQL
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVLEGES ON MySQL TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
