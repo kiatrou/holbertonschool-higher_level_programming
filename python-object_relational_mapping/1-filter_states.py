99e8831345bb81382d704fb15a8c71637a57baef
Kassandra <kassandraiatrou@gmail.com>
2025-07-01 12:34:27 +1000

    task 1 lists all states with a name starting with N from database hbtn_0d_usa

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+This is a script that lists all states with a name starting with
+an N (uppercase N) from the database hbtn_0d_usa
+"""
+
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    # Command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connect to the database
+    connection = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    # LIKE is used for pattern matching
+    # 'N%' means starts with N - % is a wildcard that matches any
+    # sequence of characters
+
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+
+    # Clean up
+    cursor.close()
+    connection.close()
