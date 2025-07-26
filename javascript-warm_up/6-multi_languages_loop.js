eab03a05b43e4f6481feca4c68e01a2b6a46f91a
Kassandra <kassandraiatrou@gmail.com>
2025-07-26 11:19:54 +1000

    a script that prints 3 lines by using an array of string and a loop

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Create an array of strings containing the messages
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+// Use a for loop to iterate through the array and print each message
+for (let i = 0; i < languages.length; i++) {
+  console.log(languages[i]);
+}
