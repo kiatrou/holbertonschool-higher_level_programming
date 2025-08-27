762eff8088a8e322e1bc76cab220b4964d457057
Kassandra <kassandraiatrou@gmail.com>
2025-08-27 11:10:23 +1000

    task 0

---
+++ javascript-dom_manipulaition/0-script.js
@@ -0,0 +1,5 @@
+document.querySelector("header").style.color = "#FF0000";
+
+// querySelector() - returns the first element that matches a CSS selector
+//style - returns the value of an element's style attribute
+// ie style.color will change the color of the selected 
