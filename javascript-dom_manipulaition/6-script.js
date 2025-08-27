1b6c34ed036bd45f6b574af085410f645d1084d3
Kassandra <kassandraiatrou@gmail.com>
2025-08-27 15:57:14 +1000

    task 6

---
+++ javascript-dom_manipulaition/6-script.js
@@ -0,0 +1,9 @@
+const character = document.getElementById("character");
+
+fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    character.textContent = data.name;
+  });
